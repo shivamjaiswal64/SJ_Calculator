03276c199bb6c45795b5924359a9f1c13158d292
shiva <shiva@DESKTOP-LHUKHFS>
2020-05-24 16:03:15 +0530

    first commit

---
+++ src/sj_calculator/Calculator.java
@@ -0,0 +1,25 @@
+/*
+ * This BMI calculator is made by Shivam Jaiswal
+ * Java Technology is used in the development of it.
+ * All rights reserved
+ */
+package sj_calculator;
+
+/**
+ *
+ * @author shivam jaiswal
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        BMI_Calc bc = new BMI_Calc();
+        bc.setVisible(true);
+        bc.pack();
+        bc.setLocationRelativeTo(null);
+    }
+    
+}
